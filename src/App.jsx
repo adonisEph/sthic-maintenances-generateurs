4e0cefff8bd8ba74527b79a2cfc87c5f958b8285
James <128242583+adonisEph@users.noreply.github.com>
2026-02-19 19:46:31 +0100

    Update changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ import {
   getUrgencyClass
 } from './utils/calculations';
 
-const APP_VERSION = '2.3.5';
+const APP_VERSION = '2.3.6';
 const APP_VERSION_STORAGE_KEY = 'gma_app_version_seen';
 const APP_VERSION_SNOOZED_AT_KEY = 'gma_app_update_snoozed_at';
 const APP_VERSION_DISMISSED_KEY = 'gma_app_update_dismissed_for';
